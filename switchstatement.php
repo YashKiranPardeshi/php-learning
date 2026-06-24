b6180673f641459b97be379dcc9bb2ba393af6a6
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-25 00:04:45 +0530

    Added PHP Switch Statement Program
    This program demonstrates the use of the Switch Statement in PHP. It checks the value of a variable and executes the matching case. Comments are included for better understanding and learning.

---
+++ switchstatement.php
@@ -0,0 +1,22 @@
+<?php // PHP code starts from here
+ $day = 3; // Creates a variable named day and stores value 3
+
+ switch($day) // Checks value using switch statement
+ {
+    case 1:
+        echo "Monday";
+        break;
+    case 2:
+        echo "Tuesday";
+        break;
+    case 3:
+        echo "Wednesday";
+        break;
+
+    default;
+        echo "Invalid day";
+ }
+
+// PHP code ends here
+
+?>
