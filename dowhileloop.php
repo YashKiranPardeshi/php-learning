159c129247516eaa8179bcbbb946027ba5375185
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-25 00:09:06 +0530

    Added PHP Do While Loop Program
    This program demonstrates the use of the Do While Loop in PHP. It executes the code block at least once before checking the condition and prints numbers from 1 to 5. Comments are included for better understanding.

---
+++ dowhileloop.php
@@ -0,0 +1,13 @@
+<?php
+
+$i = 1;// Initializes variable i with value 1
+
+do 
+// Executes code first and checks condition later
+{
+    echo $i . "<br>";  // Displays current value
+    $i++;     // Increases value by 1
+}
+while($i <= 5);
+
+?>
