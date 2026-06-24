3ef7ae66a43b49ba518f70228a841f492dd8cf7d
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-24 23:59:59 +0530

    Added PHP If Else Statement Program
    PHP If Else Statement program with comments and explanation.

---
+++ ifelsestatement.php
@@ -0,0 +1,14 @@
+<?php // PHP code starts from here
+
+$num = 5; // Creates a variable named num and stores value 5
+if($num % 2 == 0) // Checks whether number is divisible by 2
+    {
+
+      echo "Even Number"; // Displays Even Number if condition is true
+    }
+    else
+    {
+         echo "Odd Number";  // Displays Odd Number if condition is false
+    }
+// PHP code ends here
+?>
