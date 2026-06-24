b29d752318d1437350be4168f1c347f826640b71
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-24 23:58:01 +0530

    Added if statement program with comments
    PHP If Statement program with line-by-line explanation and comments.

---
+++ ifstatement.php
@@ -0,0 +1,12 @@
+<?php //→ PHP code starts from here.
+
+$age = 20; //→ Creates a variable named age and stores value 20.
+
+if($age >= 18 ) //→ Checks whether age is 18 or greater.
+    { //→ Beginning of if block.
+            echo "you are eligible to vote"; //→ Displays the output message.
+    } //→ End of if block.
+
+    
+    //→ End of PHP code
+?> 
