ea7f572d7d3ae14bbd1c35cb3c0d68cb10dc6c26
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-25 00:02:58 +0530

    Added PHP Else If Statement Program
    PHP Else If Statement program with comments and line-by-line explanation.

---
+++ elseifstatement.php
@@ -0,0 +1,24 @@
+<?php 
+// PHP code starts from here
+$marks = 75; // Creates a variable named marks and stores value 75
+
+if($marks >= 90) // Checks marks and assigns grade
+{
+        echo "Grade A";
+}
+elseif($marks >= 70)
+{
+    echo "Grade B";
+}
+elseif($marks >= 50)
+{
+    echo "Grade C";
+}
+else
+{
+    echo "fail";
+}
+
+
+
+?>
