cb6420c3572428129656e150bd468ad210b2073b
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-25 00:07:31 +0530

    Added PHP While Loop Program
    This program demonstrates the use of the While Loop in PHP. It prints numbers from 1 to 5 by repeatedly executing code while the specified condition remains true. Comments are included for better understanding.

---
+++ whileloop.php
@@ -0,0 +1,11 @@
+<?php
+
+$i = 1; // Initializes variable i with value 1
+
+while($i <= 5)// Loop runs while condition is true
+    {
+
+        echo $i . "<br>"; // Displays current value
+        $i++;     // Increases value by 1
+    }
+?>
