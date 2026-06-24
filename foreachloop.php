6b977254be99bf987cefff2f0968b2ebdeda8c6d
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-25 00:10:39 +0530

    Added PHP Foreach Loop Program
    This program demonstrates the use of the Foreach Loop in PHP. It is used to iterate through array elements and display each value one by one. Comments are included for better understanding and learning purposes.

---
+++ foreachloop.php
@@ -0,0 +1,10 @@
+<?php
+
+$fruits = array("Apple", "Mango", "Banana"); // Creates an array of fruits
+
+foreach($fruits as $fruit) // Accesses each element of array
+    {
+        echo $fruit . "<br>";   // Displays fruit name
+    }
+
+?>
